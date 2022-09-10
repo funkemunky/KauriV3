69317d58bee85cf11d6b0772e682d93a0b41ed8c
Dawson <30784509+funkemunky@users.noreply.github.com>
2022-09-10 10:38:38 -0400

    Implements BadPackets (PITCH) check

--- src/main/java/dev/brighten/ac/check/impl/packet/badpackets/Pitch.java
+++ src/main/java/dev/brighten/ac/check/impl/packet/badpackets/Pitch.java
@@ -1,4 +1,23 @@
 package dev.brighten.ac.check.impl.packet.badpackets;
 
-public class Pitch {
+import dev.brighten.ac.api.check.CheckType;
+import dev.brighten.ac.check.Check;
+import dev.brighten.ac.check.CheckData;
+import dev.brighten.ac.check.WAction;
+import dev.brighten.ac.data.APlayer;
+import dev.brighten.ac.packet.wrapper.in.WPacketPlayInFlying;
+import dev.brighten.ac.utils.annotation.Async;
+
+@CheckData(name = "BadPackets (Pitch)", checkId = "badpacketspitch", type = CheckType.BADPACKETS, punishVl = 1)
+public class Pitch extends Check {
+    public Pitch(APlayer player) {
+        super(player);
+    }
+
+    @Async
+    WAction<WPacketPlayInFlying> flying = packet -> {
+        if(packet.isLooked() && Math.abs(packet.getPitch()) > 90) {
+            flag("pitch=%.2f", Math.abs(packet.getPitch()));
+        }
+    };
 }
