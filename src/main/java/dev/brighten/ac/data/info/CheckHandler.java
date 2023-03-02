8adceeea8d4c87d55e18fa17e4953205915bf2e3
Dawson <30784509+funkemunky@users.noreply.github.com>
2023-03-02 11:20:59 -0500

    Adding pre-runtime cancellable packets

--- src/main/java/dev/brighten/ac/data/info/CheckHandler.java
+++ src/main/java/dev/brighten/ac/data/info/CheckHandler.java
@@ -15,9 +15,10 @@ import java.util.*;
 
 @RequiredArgsConstructor
 public class CheckHandler {
-    private final Map<Class<?>, ActionStore[]> events = new HashMap<>();
-    private final Map<Class<?>, TimedActionStore[]> eventsWithTimestamp = new HashMap<>();
-    private final Map<Class<?>, CancellableActionStore[]> cancellableEvents = new HashMap<>();
+    private final Map<Class<?>, ActionStore<?>[]> EVENTS = new HashMap<>();
+    private final Map<Class<?>, TimedActionStore<?>[]> EVENTS_TIMESTAMP = new HashMap<>();
+    private final Map<Class<?>, CancellableActionStore<?>[]> EVENTS_CANCELLABLE = new HashMap<>();
+    private final Map<Class<?>, CancellableActionStore<?>[]> EVENTS_PRE_CANCELLABLE = new HashMap<>();
 
     private final Map<Class<? extends Check>, Check> checkCache = new HashMap<>();
 
@@ -65,11 +66,11 @@ public class CheckHandler {
 
             checks.add(check);
 
-            synchronized (events) {
+            synchronized (EVENTS) {
                 for (Tuple<WrappedField, Class<?>> tuple : checkClass.getActions()) {
                     WAction<?> action = tuple.one.get(check);
 
-                    events.compute(tuple.two, (packetClass, array) -> {
+                    EVENTS.compute(tuple.two, (packetClass, array) -> {
                         if (array == null) {
                             return new ActionStore[] {new ActionStore(action, checkClass.getCheckClass().getParent())};
                         } else {
@@ -80,26 +81,28 @@ public class CheckHandler {
                     });
                 }
             }
-            synchronized (eventsWithTimestamp) {
+            synchronized (EVENTS_TIMESTAMP) {
                 for (Tuple<WrappedField, Class<?>> tuple : checkClass.getTimedActions()) {
                     WTimedAction<?> action = tuple.one.get(check);
 
-                    eventsWithTimestamp.compute(tuple.two, (packetClass, array) -> {
+                    EVENTS_TIMESTAMP.compute(tuple.two, (packetClass, array) -> {
                         if (array == null) {
                             return new TimedActionStore[] {new TimedActionStore(action, checkClass.getCheckClass().getParent())};
                         } else {
-                            TimedActionStore[] newArray = Arrays.copyOf(array, array.length + 1);
+                            TimedActionStore<?>[] newArray = Arrays.copyOf(array, array.length + 1);
                             newArray[array.length] = new TimedActionStore(action, checkClass.getCheckClass().getParent());
                             return newArray;
                         }
                     });
                 }
             }
-            synchronized (cancellableEvents) {
+            synchronized (EVENTS_CANCELLABLE) {
                 for (Tuple<WrappedField, Class<?>> tuple : checkClass.getCancellableActions()) {
                     WCancellable<?> action = tuple.one.get(check);
 
-                    cancellableEvents.compute(tuple.two, (packetClass, array) -> {
+                    if(tuple.one.isAnnotationPresent(Pre.class)) continue;
+
+                    EVENTS_CANCELLABLE.compute(tuple.two, (packetClass, array) -> {
                         if (array == null) {
                             return new CancellableActionStore[] {new CancellableActionStore(action, checkClass.getCheckClass().getParent())};
                         } else {
@@ -110,14 +113,17 @@ public class CheckHandler {
                     });
                 }
             }
+            synchronized (EVENTS_PRE_CANCELLABLE) {
+                
+            }
         }
     }
 
     public void shutdown() {
         checks.clear();
-        events.clear();
-        eventsWithTimestamp.clear();
-        cancellableEvents.clear();
+        EVENTS.clear();
+        EVENTS_TIMESTAMP.clear();
+        EVENTS_CANCELLABLE.clear();
     }
 
     public void disableCheck(String checkName) {
@@ -125,8 +131,8 @@ public class CheckHandler {
     }
 
     public void callEvent(Event event) {
-        if(events.containsKey(event.getClass())) {
-            ActionStore<Event>[] actions = (ActionStore<Event>[]) events.get(event.getClass());
+        if(EVENTS.containsKey(event.getClass())) {
+            ActionStore<Event>[] actions = (ActionStore<Event>[]) EVENTS.get(event.getClass());
             for (ActionStore<Event> action : actions) {
                 if(!Anticheat.INSTANCE.getCheckManager().getCheckSettings(action.getCheckClass()).isEnabled())
                     continue;
@@ -138,9 +144,9 @@ public class CheckHandler {
     //TODO When using WPacket wrappers only, make this strictly WPacket param based only
 
     public boolean callSyncPacket(Object packet, long timestamp) {
-        if(events.containsKey(packet.getClass())) {
-            synchronized (events) {
-                ActionStore<Object>[] actions = events.get(packet.getClass());
+        if(EVENTS.containsKey(packet.getClass())) {
+            synchronized (EVENTS) {
+                ActionStore<Object>[] actions = (ActionStore<Object>[]) EVENTS.get(packet.getClass());
                 for (ActionStore<Object> action : actions) {
                     if(!Anticheat.INSTANCE.getCheckManager().getCheckSettings(action.getCheckClass()).isEnabled())
                         continue;
@@ -148,9 +154,10 @@ public class CheckHandler {
                 }
             }
         }
-        if(eventsWithTimestamp.containsKey(packet.getClass())) {
-            synchronized (eventsWithTimestamp) {
-                TimedActionStore<Object>[] actions = eventsWithTimestamp.get(packet.getClass());
+        if(EVENTS_TIMESTAMP.containsKey(packet.getClass())) {
+            synchronized (EVENTS_TIMESTAMP) {
+                TimedActionStore<Object>[] actions = (TimedActionStore<Object>[])
+                        EVENTS_TIMESTAMP.get(packet.getClass());
                 for (TimedActionStore<Object> action : actions) {
                     if(!Anticheat.INSTANCE.getCheckManager().getCheckSettings(action.getCheckClass()).isEnabled())
                         continue;
@@ -158,10 +165,11 @@ public class CheckHandler {
                 }
             }
         }
-        if(cancellableEvents.containsKey(packet.getClass())) {
+        if(EVENTS_CANCELLABLE.containsKey(packet.getClass())) {
             boolean cancelled = false;
-            synchronized (cancellableEvents) {
-                CancellableActionStore<Object>[] actions = cancellableEvents.get(packet.getClass());
+            synchronized (EVENTS_CANCELLABLE) {
+                CancellableActionStore<Object>[] actions = (CancellableActionStore<Object>[])
+                        EVENTS_CANCELLABLE.get(packet.getClass());
                 for (CancellableActionStore<Object> action : actions) {
                     if(!Anticheat.INSTANCE.getCheckManager().getCheckSettings(action.getCheckClass()).isEnabled())
                         continue;
