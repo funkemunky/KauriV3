ed227fcaf14c64197a922da093a4c5ca88cb23f0
Dawson <dawson@funkemunky.cc>
2025-06-22 11:36:38 -0400

    Fixing shit

---
+++ Anticheat/src/main/java/dev/brighten/ac/handler/protocol/ViaVersionAPI.java
@@ -0,0 +1,18 @@
+package dev.brighten.ac.handler.protocol;
+
+import com.viaversion.viaversion.api.Via;
+import dev.brighten.ac.Anticheat;
+import dev.brighten.ac.handler.protocolsupport.Protocol;
+import org.bukkit.entity.Player;
+
+public class ViaVersionAPI implements Protocol {
+
+    @Override
+    public int getPlayerVersion(Player player) {
+        Anticheat.INSTANCE.alog("Getting player version for " + player.getName());
+        var toReturn = Via.getAPI().getPlayerVersion(player.getUniqueId());
+        Anticheat.INSTANCE.alog("Player version for " + player.getName() + " is " + toReturn);
+
+        return toReturn;
+    }
+}
